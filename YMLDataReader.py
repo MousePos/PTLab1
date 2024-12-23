6ddf4c32ab4a57d9f3595a52998c918da0a688e6
MousePos <125408673+MousePos@users.noreply.github.com>
2024-12-23 11:15:12 +0300

    Delete YMLDataReader.py

--- YMLDataReader.py
+++
@@ -1,9 +0,0 @@
-import yaml
-from typing import List, Dict
-from DataReader import DataReader
-
-class YamlDataReader(DataReader):
-    def read(self, path: str) -> List[Dict[str, Dict[str, int]]]:
-        with open(path, 'r', encoding='utf-8') as file:
-            data = yaml.safe_load(file)
-        return data
